0847316afcee3ab4c8c202fa59d26a67ebe95ccf
mcxleague <119989048+mcxleague@users.noreply.github.com>
2024-01-19 15:58:09 -0800

    Update layout.tsx

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -3,6 +3,10 @@ import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function RootLayout({
+    children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en" className={cn(satoshi.variable, inter.variable)}>
     <body>
