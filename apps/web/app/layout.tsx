903892c65649caca3c5e65979953a49e17ef44ac
mcxleague <119989048+mcxleague@users.noreply.github.com>
2024-01-19 15:51:34 -0800

    Update layout.tsx

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,28 +1,20 @@
-import { inter, satoshi } from "@/styles/fonts";
-import "@/styles/globals.css";
-import { cn, constructMetadata } from "@dub/utils";
-import { TooltipProvider } from "@dub/ui/src/tooltip";
-import { Toaster } from "sonner";
+import { NavMobile, Nav, Footer } from "@dub/ui";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
-export const metadata = constructMetadata();
-
 export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
   return (
     <html lang="en" className={cn(satoshi.variable, inter.variable)}>
-      <body>
-        <TooltipProvider>
-          <Toaster closeButton />
-          {children}
-          <Analytics />
-          <SpeedInsights />
-        </TooltipProvider>
-      </body>
-    </html>
+    <body>
+    <div className="flex min-h-screen flex-col justify-between">
+      <NavMobile />
+      <Nav />
+      {children}
+      <Analytics />
+       <SpeedInsights />
+      <Footer />
+    </div>
+  </body>
+  </html>
   );
 }
