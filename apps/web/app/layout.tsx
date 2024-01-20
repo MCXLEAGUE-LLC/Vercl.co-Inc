a2ac13d8168ed2752c851a66dcee2c48c0e9bc49
mcxleague <119989048+mcxleague@users.noreply.github.com>
2024-01-20 08:06:16 -0800

    Update layout.tsx

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,7 +1,7 @@
 import { NavMobile, Nav, Footer } from "@dub/ui";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import { satoshi } from "@/styles/fonts";
+import { satoshi, inter } from "@/styles/fonts";
 import { cn, constructMetadata } from "@dub/utils";
 
 export const metadata = {
