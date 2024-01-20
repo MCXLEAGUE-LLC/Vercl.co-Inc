2f7236adc3c17d42a0fa5ca6c97f6e401a1d846f
mcxleague <119989048+mcxleague@users.noreply.github.com>
2024-01-19 21:03:15 -0800

    Update page.tsx

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -22,6 +22,8 @@ export default function PlaceholderContent() {
   const [showText] = useDebounce(loading ? false : true, 800);
 
   return (
+    <>
+      <div className="z-10 w-full max-w-xl px-5 xl:px-0">
         <h1
           className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:text-7xl md:leading-[5rem]"
           style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
@@ -34,6 +36,6 @@ export default function PlaceholderContent() {
         >
           Vercl.co is the go-to link management infrastructure for modern marketing teams.
         </p>
-    ); 
+    </>
   );
 }
