4a5dd6eb64e028b649a144c14dda532e39bf606b
mcxleague <119989048+mcxleague@users.noreply.github.com>
2024-01-19 20:50:52 -0800

    Update page.tsx

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -24,15 +24,17 @@ export default function PlaceholderContent() {
   return (
 <div>
     <Background />
-    <div className="z-10 w-full max-w-xl px-5 xl:px-0">
-      <h1
-        className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:text-7xl md:leading-[5rem]"
-        style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
-      >
-        Short Links With Superpowers
-      </h1>
-      <p className="mt-6 animate-fade-up text-center text-gray-500 opacity-0 [text-wrap:balance] md:text-xl" style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}>Vercl.co is the go-to link management infrastructure for modern marketing teams.</p>
-    </div>
-</div>
+        <h1
+          className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:text-7xl md:leading-[5rem]"
+          style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
+        >
+          Short Links With Superpowers
+        </h1>
+        <p
+          className="mt-6 animate-fade-up text-center text-gray-500 opacity-0 [text-wrap:balance] md:text-xl"
+          style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
+        >
+          Vercl.co is the go-to link management infrastructure for modern marketing teams.
+        </p>
   );
 }
