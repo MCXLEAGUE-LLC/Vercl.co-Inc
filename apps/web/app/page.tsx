f26bace3d3a617be6eb86d7a268ab378ff7c2cf4
mcxleague <119989048+mcxleague@users.noreply.github.com>
2024-01-20 08:28:42 -0800

    Update page.tsx

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -8,12 +8,11 @@ import { motion } from "framer-motion";
 import { useParams } from "next/navigation";
 import { useState } from "react";
 import { useDebounce } from "use-debounce";
-import { FEATURES_LIST, Background, Form } from "@dub/ui";
+import { Background, Form } from "@dub/ui";
 
 export default async function Home() {
   return (
     <>
-      <FEATURES_LIST />
       <Background />
       <Form />
     </>
