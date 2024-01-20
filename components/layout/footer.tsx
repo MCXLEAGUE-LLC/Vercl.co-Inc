a53aed297ce5823aa6a0b1675a916d37a07ca674
mcxleague <119989048+mcxleague@users.noreply.github.com>
2024-01-19 16:56:18 -0800

    Add files via upload

---
+++ components/layout/footer.tsx
@@ -0,0 +1,241 @@
+import { BuyMeACoffee } from "../shared/icons";
+
+export default function Footer() {
+  return (
+<footer className="z-10 border-t border-gray-200 bg-white/50 py-8 backdrop-blur-lg">
+  <div className="mx-auto w-full max-w-screen-xl px-2.5 lg:px-20 pt-10">
+    <div className="xl:grid xl:grid-cols-5 xl:gap-8">
+      <div className="space-y-8 xl:col-span-2">
+        <a href="/web/20230910002640/https://dub.co/">
+          <span className="sr-only">Dub.co Logo</span>
+          <svg
+            width={834}
+            height={236}
+            viewBox="0 0 834 236"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-24 h-7 text-gray-600"
+          >
+            <path
+              d="M266 119.5C266 181.632 215.632 232 153.5 232C91.368 232 41 181.632 41 119.5C41 57.368 91.368 7 153.5 7C215.632 7 266 57.368 266 119.5Z"
+              fill="currentColor"
+            />
+            <mask
+              id="mask0_46_2"
+              style={{ maskType: "alpha" }}
+              maskUnits="userSpaceOnUse"
+              x={41}
+              y={7}
+              width={225}
+              height={225}
+            >
+              <path
+                d="M266 119.5C266 181.632 215.632 232 153.5 232C91.368 232 41 181.632 41 119.5C41 57.368 91.368 7 153.5 7C215.632 7 266 57.368 266 119.5Z"
+                fill="currentColor"
+              />
+            </mask>
+            <g mask="url(#mask0_46_2)">
+              <ellipse cx={153} cy="124.5" rx={58} ry="57.5" fill="white" />
+              <path d="M185 -10H211V181H185V-10Z" fill="white" />
+            </g>
+            <path
+              d="M397.25 213.75C385.25 213.75 374.833 211.083 366 205.75C357.167 200.417 350.25 192.917 345.25 183.25C340.417 173.417 338 161.917 338 148.75C338 135.417 340.5 123.75 345.5 113.75C350.5 103.583 357.583 95.6667 366.75 90C376.083 84.3333 387.083 81.5 399.75 81.5C407.583 81.5 414.833 82.9167 421.5 85.75C428.333 88.5833 433.583 92.3333 437.25 97V22H475.75V210.5H439.5L437.5 194.5C434.167 200.167 428.833 204.833 421.5 208.5C414.333 212 406.25 213.75 397.25 213.75ZM406.5 178.5C412.5 178.5 417.75 177.25 422.25 174.75C426.917 172.083 430.5 168.417 433 163.75C435.667 158.917 437 153.417 437 147.25C437 140.917 435.667 135.417 433 130.75C430.5 126.083 426.917 122.5 422.25 120C417.75 117.333 412.5 116 406.5 116C400.5 116 395.25 117.333 390.75 120C386.25 122.667 382.75 126.333 380.25 131C377.917 135.667 376.75 141.083 376.75 147.25C376.75 153.417 377.917 158.833 380.25 163.5C382.75 168.167 386.25 171.833 390.75 174.5C395.25 177.167 400.5 178.5 406.5 178.5Z"
+              fill="currentColor"
+            />
+            <path
+              d="M587.662 85.5H626.162C626.162 85.5 626.162 148.5 626.162 164C626.162 179.5 623.786 191.5 616.5 200.5C609.214 209.5 594 213.75 582.162 213.75C570.324 213.75 564.412 213.75 564.412 213.75C564.412 213.75 556.662 213.75 549.162 213.75C534.995 213.75 523.745 209.333 515.412 200.5C507.245 191.5 503.162 179.333 503.162 164V85.5H541.662V150C541.662 160.167 543.495 167.667 547.162 172.5C550.829 177.167 556.579 179.5 564.412 179.5C572.745 179.5 578.662 177.25 582.162 172.75C585.829 168.083 587.662 160.667 587.662 150.5V85.5Z"
+              fill="currentColor"
+            />
+            <path
+              d="M691.303 210.5H655.053V22H693.553V100C697.386 94.3333 703.053 89.8333 710.553 86.5C718.219 83 726.553 81.25 735.553 81.25C747.219 81.25 757.303 84.0833 765.803 89.75C774.469 95.4167 781.136 103.333 785.803 113.5C790.469 123.667 792.803 135.583 792.803 149.25C792.803 162.083 790.219 173.417 785.053 183.25C780.053 192.917 772.969 200.417 763.803 205.75C754.803 211.083 744.386 213.75 732.553 213.75C723.886 213.75 715.969 212 708.803 208.5C701.803 204.833 696.636 200.167 693.303 194.5L691.303 210.5ZM693.803 147.25C693.803 153.417 695.053 158.917 697.553 163.75C700.219 168.417 703.803 172.083 708.303 174.75C712.969 177.25 718.303 178.5 724.303 178.5C730.469 178.5 735.719 177.167 740.053 174.5C744.553 171.833 747.969 168.167 750.303 163.5C752.803 158.833 754.053 153.417 754.053 147.25C754.053 141.083 752.803 135.667 750.303 131C747.969 126.333 744.553 122.667 740.053 120C735.719 117.333 730.469 116 724.303 116C718.303 116 712.969 117.333 708.303 120C703.803 122.5 700.219 126.083 697.553 130.75C695.053 135.417 693.803 140.917 693.803 147.25Z"
+              fill="currentColor"
+            />
+          </svg>
+        </a>
+        <p className="max-w-xs text-sm text-gray-500">
+          Giving modern marketing teams superpowers with short links that stand
+          out.
+        </p>
+        <div className="flex items-center space-x-2">
+          <a
+            href="https://web.archive.org/web/20230910002640/https://twitter.com/dubdotco"
+            target="_blank"
+            rel="noreferrer"
+            className="group rounded-md p-2 transition-colors hover:bg-gray-100 active:bg-gray-200"
+          >
+            <span className="sr-only">Twitter</span>
+            <div className="group">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 248 204"
+                className="group-hover:hidden h-5 w-5 text-gray-600"
+              >
+                <path
+                  fill="currentColor"
+                  d="M221.95 51.29c.15 2.17.15 4.34.15 6.53 0 66.73-50.8 143.69-143.69 143.69v-.04c-27.44.04-54.31-7.82-77.41-22.64 3.99.48 8 .72 12.02.73 22.74.02 44.83-7.61 62.72-21.66-21.61-.41-40.56-14.5-47.18-35.07 7.57 1.46 15.37 1.16 22.8-.87-23.56-4.76-40.51-25.46-40.51-49.5v-.64c7.02 3.91 14.88 6.08 22.92 6.32C11.58 63.31 4.74 33.79 18.14 10.71c25.64 31.55 63.47 50.73 104.08 52.76-4.07-17.54 1.49-35.92 14.61-48.25 20.34-19.12 52.33-18.14 71.45 2.19 11.31-2.23 22.15-6.38 32.07-12.26-3.77 11.69-11.66 21.62-22.2 27.93 10.01-1.18 19.79-3.86 29-7.95-6.78 10.16-15.32 19.01-25.2 26.16z"
+                />
+              </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 1668.56 1221.19"
+                className="hidden scale-150 group-hover:block h-5 w-5 text-gray-600"
+              >
+                <g transform="translate(52.390088,-25.058597)">
+                  <path
+                    fill="currentColor"
+                    d="M283.94,167.31l386.39,516.64L281.5,1104h87.51l340.42-367.76L984.48,1104h297.8L874.15,558.3l361.92-390.99   h-87.51l-313.51,338.7l-253.31-338.7H283.94z M412.63,231.77h136.81l604.13,807.76h-136.81L412.63,231.77z"
+                  />
+                </g>
+              </svg>
+            </div>
+          </a>
+          <div className="h-8 border-l border-gray-200" />
+          <a
+            href="https://web.archive.org/web/20230910002640/https://github.com/steven-tey/dub"
+            target="_blank"
+            rel="noreferrer"
+            className="rounded-md p-2 transition-colors hover:bg-gray-100 active:bg-gray-200"
+          >
+            <span className="sr-only">Github</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width={20}
+              height={20}
+              fill="currentColor"
+              viewBox="0 0 24 24"
+              className="h-5 w-5 text-gray-600"
+            >
+              <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+            </svg>
+          </a>
+          <div className="h-8 border-l border-gray-200" />
+          <a
+            href="https://web.archive.org/web/20230910002640/https://www.linkedin.com/company/dubhq/"
+            target="_blank"
+            rel="noreferrer"
+            className="rounded-md p-2 transition-colors hover:bg-gray-100 active:bg-gray-200"
+          >
+            <span className="sr-only">LinkedIn</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width={24}
+              height={24}
+              viewBox="0 0 24 24"
+              className="h-5 w-5"
+            >
+              <path
+                fill="#52525B"
+                d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"
+              />
+            </svg>
+          </a>
+        </div>
+      </div>
+      <div className="mt-16 grid grid-cols-2 gap-8 xl:col-span-3 xl:mt-0">
+        <div className="md:grid md:grid-cols-2 md:gap-8">
+          <div>
+            <h3 className="text-sm font-semibold text-gray-900">Product</h3>
+            <ul role="list" className="mt-4 space-y-4">
+              <li>
+                <a
+                  className="text-sm text-gray-500 hover:text-gray-900"
+                  href="/web/20230910002640/https://dub.co/pricing"
+                >
+                  Pricing
+                </a>
+              </li>
+              <li>
+                <a
+                  className="text-sm text-gray-500 hover:text-gray-900"
+                  href="/web/20230910002640/https://dub.co/changelog"
+                >
+                  Changelog
+                </a>
+              </li>
+              <li>
+                <a
+                  className="text-sm text-gray-500 hover:text-gray-900"
+                  href="/web/20230910002640/https://dub.co/help"
+                >
+                  Help Center
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="mt-10 md:mt-0">
+            <h3 className="text-sm font-semibold text-gray-600">Company</h3>
+            <ul role="list" className="mt-4 space-y-4">
+              <li>
+                <a
+                  className="text-sm text-gray-500 hover:text-gray-900"
+                  href="/web/20230910002640/https://dub.co/blog"
+                >
+                  Blog
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="md:grid md:grid-cols-2 md:gap-8">
+          <div>
+            <h3 className="text-sm font-semibold text-gray-600">Tools</h3>
+            <ul role="list" className="mt-4 space-y-4">
+              <li>
+                <a
+                  className="text-sm text-gray-500 hover:text-gray-900"
+                  href="/web/20230910002640/https://dub.co/tools/metatags"
+                >
+                  Metatags API
+                </a>
+              </li>
+              <li>
+                <a
+                  className="text-sm text-gray-500 hover:text-gray-900"
+                  href="/web/20230910002640/https://dub.co/tools/inspector"
+                >
+                  Link Inspector
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="mt-10 md:mt-0">
+            <h3 className="text-sm font-semibold text-gray-600">Legal</h3>
+            <ul role="list" className="mt-4 space-y-4">
+              <li>
+                <a
+                  className="text-sm text-gray-500 hover:text-gray-900"
+                  href="/web/20230910002640/https://dub.co/privacy"
+                >
+                  Privacy
+                </a>
+              </li>
+              <li>
+                <a
+                  className="text-sm text-gray-500 hover:text-gray-900"
+                  href="/web/20230910002640/https://dub.co/terms"
+                >
+                  Terms
+                </a>
+              </li>
+              <li>
+                <a
+                  className="text-sm text-gray-500 hover:text-gray-900"
+                  href="/web/20230910002640/https://dub.co/abuse"
+                >
+                  Abuse
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="mt-16 border-t border-gray-900/10 pt-8 sm:mt-20 lg:mt-24">
+      <p className="text-sm leading-5 text-gray-500">
+        © {/* */}2023{/* */} Vercl.co
+      </p>
+    </div>
+  </div>
+</footer>
+  );
+}
