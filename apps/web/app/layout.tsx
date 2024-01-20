f42b9ed1490ebb371f41b5f18e5c809a28e37d24
mcxleague <119989048+mcxleague@users.noreply.github.com>
2024-01-20 07:32:31 -0800

    Update layout.tsx

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -3,7 +3,10 @@ import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { inter, satoshi } from "@/styles/fonts";
 import { cn, constructMetadata } from "@dub/utils";
-import "@/styles/globals.css";
+import "./globals.css";
+import cx from "classnames";
+import { sfPro, inter } from "./fonts";
+
 export const metadata = {
   title: "Vercl.co - is the go-to link management infrastructure for modern marketing teams.",
   description:
