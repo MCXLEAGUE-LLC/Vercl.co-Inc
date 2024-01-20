bccce43e0a843c0f4dfc3f66b97339d3b291a302
mcxleague <119989048+mcxleague@users.noreply.github.com>
2024-01-19 16:00:39 -0800

    Update layout.tsx

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,8 @@
 import { NavMobile, Nav, Footer } from "@dub/ui";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { inter, satoshi } from "@/styles/fonts";
+import { cn, constructMetadata } from "@dub/utils";
 
 export default function RootLayout({
     children,
