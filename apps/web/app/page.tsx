0eb1192ab172835f1f5adc4821d9826a1492b76b
mcxleague <119989048+mcxleague@users.noreply.github.com>
2024-01-19 20:51:55 -0800

    Update page.tsx

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -22,7 +22,6 @@ export default function PlaceholderContent() {
   const [showText] = useDebounce(loading ? false : true, 800);
 
   return (
-<div>
     <Background />
         <h1
           className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:text-7xl md:leading-[5rem]"
