2eb65c042c9b6a0a8180a13079597029ea2b6a01
mcxleague <119989048+mcxleague@users.noreply.github.com>
2024-01-19 17:16:00 -0800

    Update page.tsx

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -9,7 +9,6 @@ import { useParams } from "next/navigation";
 import { useState } from "react";
 import { useDebounce } from "use-debounce";
 import { Background } from "@dub/ui";
-import { Github, Twitter } from "./components/shared/icons";
 
 export default function PlaceholderContent() {
   const { domain } = useParams() as { domain: string };
@@ -26,17 +25,6 @@ export default function PlaceholderContent() {
 <div>
     <Background />
     <div className="z-10 w-full max-w-xl px-5 xl:px-0">
-      <a
-        href="https://twitter.com/steventey/status/1613928948915920896"
-        target="_blank"
-        rel="noreferrer"
-        className="mx-auto mb-5 flex max-w-fit animate-fade-up items-center justify-center space-x-2 overflow-hidden rounded-full bg-blue-100 px-7 py-2 transition-colors hover:bg-blue-200"
-      >
-        <Twitter className="h-5 w-5 text-[#1d9bf0]" />
-        <p className="text-sm font-semibold text-[#1d9bf0]">
-          Introducing Vercl.co
-        </p>
-      </a>
       <h1
         className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:text-7xl md:leading-[5rem]"
         style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
