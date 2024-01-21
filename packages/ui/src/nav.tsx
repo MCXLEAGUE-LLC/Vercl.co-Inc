65f4360173afbcd52fced4ac851ff4db44d94a1d
mcxleague <119989048+mcxleague@users.noreply.github.com>
2024-01-21 11:25:54 -0800

    Update nav.tsx

--- packages/ui/src/nav.tsx
+++ packages/ui/src/nav.tsx
@@ -36,12 +36,12 @@ export const navItems = [
 ];
 
 export function Nav() {
-  const { domain = "dub.co" } = useParams() as { domain: string };
+  const { domain = "mcxleague" } = useParams() as { domain: string };
   const scrolled = useScroll(80);
   const selectedLayout = useSelectedLayoutSegment();
   const helpCenter = selectedLayout === "help";
   const { data: session, isLoading } = useSWR(
-    domain === "dub.co" && "/api/auth/session",
+    domain === "mcxleague.com" && "/api/auth/session",
     fetcher,
     {
       dedupingInterval: 60000,
@@ -64,8 +64,8 @@ export function Nav() {
         <div className="flex h-14 items-center justify-between">
           <div className="flex items-center space-x-4">
             <Link
-              href={domain === "dub.co" ? "/" : `https://dub.co`}
-              {...(domain !== "dub.co" && {
+              href={domain === "mcxleague" ? "/" : `https://mcxleague`}
+              {...(domain !== "mcxleague" && {
                 onClick: () => {
                   va.track("Referred from custom domain", {
                     domain,
@@ -114,11 +114,11 @@ export function Nav() {
                             <Link
                               key={slug}
                               href={
-                                domain === "dub.co"
+                                domain === "mcxleague"
                                   ? `/${slug}`
-                                  : `https://dub.co/${slug}`
+                                  : `https://mcxleague/${slug}`
                               }
-                              {...(domain !== "dub.co" && {
+                              {...(domain !== "mcxleague" && {
                                 onClick: () => {
                                   va.track("Referred from custom domain", {
                                     domain,
@@ -150,11 +150,11 @@ export function Nav() {
                         id={`nav-${slug}`}
                         key={slug}
                         href={
-                          domain === "dub.co"
+                          domain === "mcxleague"
                             ? `/${slug}`
-                            : `https://dub.co/${slug}`
+                            : `https://mcxleague/${slug}`
                         }
-                        {...(domain !== "dub.co" && {
+                        {...(domain !== "mcxleague" && {
                           onClick: () => {
                             va.track("Referred from custom domain", {
                               domain,
@@ -192,7 +192,7 @@ export function Nav() {
               <>
                 <Link
                   href={`${APP_DOMAIN}/login`}
-                  {...(domain !== "dub.co" && {
+                  {...(domain !== "mcxleague" && {
                     onClick: () => {
                       va.track("Referred from custom domain", {
                         domain,
@@ -206,7 +206,7 @@ export function Nav() {
                 </Link>
                 <Link
                   href={`${APP_DOMAIN}/register`}
-                  {...(domain !== "dub.co" && {
+                  {...(domain !== "mcxleague" && {
                     onClick: () => {
                       va.track("Referred from custom domain", {
                         domain,
