64a982c34d5f05be39d61b6a612a42c273656fde
mcxleague <119989048+mcxleague@users.noreply.github.com>
2024-01-21 10:42:23 -0800

    Update page.tsx

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -167,6 +167,279 @@ export default async function Home() {
   </p>
 </div>
   </div>
+      <div className="mx-auto w-full max-w-screen-xl px-2.5 lg:px-20 space-y-5 pt-20 md:space-y-10">
+  <div className="mx-auto max-w-md text-center sm:max-w-xl">
+    <h2 className="font-display text-4xl font-extrabold leading-tight text-black sm:text-5xl sm:leading-tight">
+      We ship{/* */}{" "}
+      <span className="bg-gradient-to-br from-green-600 to-green-300 bg-clip-text pr-2 italic text-transparent">
+        fast
+      </span>
+    </h2>
+    <p className="mt-5 text-gray-600 sm:text-lg">
+      Check out our changelog to see what's new on Dub.co
+    </p>
+  </div>
+  <ul className="mx-5 max-w-2xl md:mx-auto md:translate-x-16">
+    <li>
+      <a
+        className="group hidden grid-cols-9 items-center md:grid"
+        href="/blog/new-pricing"
+      >
+        <dl className="col-span-2">
+          <dt className="sr-only">Published on</dt>
+          <dd className="text-base font-medium text-gray-400 transition-colors group-hover:text-gray-700">
+            <time dateTime="2024-01-19">January 19, 2024</time>
+          </dd>
+        </dl>
+        <div className="col-span-7 flex items-center">
+          <div className="relative ml-4">
+            <div className="h-16 border-l border-gray-400 pr-8" />
+            <div className="absolute -left-1 top-[1.6875rem] h-2.5 w-2.5 rounded-full bg-gray-400 transition-colors group-hover:bg-gray-700" />
+          </div>
+          <h3 className="text-2xl font-medium tracking-tight text-gray-700 transition-colors group-hover:text-black">
+            Pricing Updates (Jan '24)
+          </h3>
+        </div>
+      </a>
+      <a
+        className="flex items-center space-x-4 rounded-lg active:bg-gray-100 md:hidden"
+        href="/blog/new-pricing"
+      >
+        <div className="relative">
+          <div className="h-16 border-l border-gray-400" />
+          <div className="absolute -left-1 top-5 h-2.5 w-2.5 rounded-full bg-gray-400" />
+        </div>
+        <div>
+          <dl>
+            <dt className="sr-only">Published on</dt>
+            <dd className="text-sm font-medium text-gray-400">
+              <time dateTime="2024-01-19">January 19, 2024</time>
+            </dd>
+          </dl>
+          <h3 className="text-lg font-medium tracking-tight text-gray-700">
+            Pricing Updates (Jan '24)
+          </h3>
+        </div>
+      </a>
+    </li>
+    <li>
+      <a
+        className="group hidden grid-cols-9 items-center md:grid"
+        href="/blog/company"
+      >
+        <dl className="col-span-2">
+          <dt className="sr-only">Published on</dt>
+          <dd className="text-base font-medium text-gray-400 transition-colors group-hover:text-gray-700">
+            <time dateTime="2024-01-17">January 17, 2024</time>
+          </dd>
+        </dl>
+        <div className="col-span-7 flex items-center">
+          <div className="relative ml-4">
+            <div className="h-16 border-l border-gray-400 pr-8" />
+            <div className="absolute -left-1 top-[1.6875rem] h-2.5 w-2.5 rounded-full bg-gray-400 transition-colors group-hover:bg-gray-700" />
+          </div>
+          <h3 className="text-2xl font-medium tracking-tight text-gray-700 transition-colors group-hover:text-black">
+            Unveiling Dub.co's New Chapter
+          </h3>
+        </div>
+      </a>
+      <a
+        className="flex items-center space-x-4 rounded-lg active:bg-gray-100 md:hidden"
+        href="/blog/company"
+      >
+        <div className="relative">
+          <div className="h-16 border-l border-gray-400" />
+          <div className="absolute -left-1 top-5 h-2.5 w-2.5 rounded-full bg-gray-400" />
+        </div>
+        <div>
+          <dl>
+            <dt className="sr-only">Published on</dt>
+            <dd className="text-sm font-medium text-gray-400">
+              <time dateTime="2024-01-17">January 17, 2024</time>
+            </dd>
+          </dl>
+          <h3 className="text-lg font-medium tracking-tight text-gray-700">
+            Unveiling Dub.co's New Chapter
+          </h3>
+        </div>
+      </a>
+    </li>
+    <li>
+      <a
+        className="group hidden grid-cols-9 items-center md:grid"
+        href="/changelog/dub-links-updates"
+      >
+        <dl className="col-span-2">
+          <dt className="sr-only">Published on</dt>
+          <dd className="text-base font-medium text-gray-400 transition-colors group-hover:text-gray-700">
+            <time dateTime="2024-01-17">January 17, 2024</time>
+          </dd>
+        </dl>
+        <div className="col-span-7 flex items-center">
+          <div className="relative ml-4">
+            <div className="h-16 border-l border-gray-400 pr-8" />
+            <div className="absolute -left-1 top-[1.6875rem] h-2.5 w-2.5 rounded-full bg-gray-400 transition-colors group-hover:bg-gray-700" />
+          </div>
+          <h3 className="text-2xl font-medium tracking-tight text-gray-700 transition-colors group-hover:text-black">
+            Updates to dub.sh Links
+          </h3>
+        </div>
+      </a>
+      <a
+        className="flex items-center space-x-4 rounded-lg active:bg-gray-100 md:hidden"
+        href="/changelog/dub-links-updates"
+      >
+        <div className="relative">
+          <div className="h-16 border-l border-gray-400" />
+          <div className="absolute -left-1 top-5 h-2.5 w-2.5 rounded-full bg-gray-400" />
+        </div>
+        <div>
+          <dl>
+            <dt className="sr-only">Published on</dt>
+            <dd className="text-sm font-medium text-gray-400">
+              <time dateTime="2024-01-17">January 17, 2024</time>
+            </dd>
+          </dl>
+          <h3 className="text-lg font-medium tracking-tight text-gray-700">
+            Updates to dub.sh Links
+          </h3>
+        </div>
+      </a>
+    </li>
+    <li>
+      <a
+        className="group hidden grid-cols-9 items-center md:grid"
+        href="/changelog/rebrandly-migration-assistant"
+      >
+        <dl className="col-span-2">
+          <dt className="sr-only">Published on</dt>
+          <dd className="text-base font-medium text-gray-400 transition-colors group-hover:text-gray-700">
+            <time dateTime="2023-12-18">December 18, 2023</time>
+          </dd>
+        </dl>
+        <div className="col-span-7 flex items-center">
+          <div className="relative ml-4">
+            <div className="h-16 border-l border-gray-400 pr-8" />
+            <div className="absolute -left-1 top-[1.6875rem] h-2.5 w-2.5 rounded-full bg-gray-400 transition-colors group-hover:bg-gray-700" />
+          </div>
+          <h3 className="text-2xl font-medium tracking-tight text-gray-700 transition-colors group-hover:text-black">
+            Rebrandly Migration Assistant
+          </h3>
+        </div>
+      </a>
+      <a
+        className="flex items-center space-x-4 rounded-lg active:bg-gray-100 md:hidden"
+        href="/changelog/rebrandly-migration-assistant"
+      >
+        <div className="relative">
+          <div className="h-16 border-l border-gray-400" />
+          <div className="absolute -left-1 top-5 h-2.5 w-2.5 rounded-full bg-gray-400" />
+        </div>
+        <div>
+          <dl>
+            <dt className="sr-only">Published on</dt>
+            <dd className="text-sm font-medium text-gray-400">
+              <time dateTime="2023-12-18">December 18, 2023</time>
+            </dd>
+          </dl>
+          <h3 className="text-lg font-medium tracking-tight text-gray-700">
+            Rebrandly Migration Assistant
+          </h3>
+        </div>
+      </a>
+    </li>
+    <li>
+      <a
+        className="group hidden grid-cols-9 items-center md:grid"
+        href="/blog/introducing-new-analytics"
+      >
+        <dl className="col-span-2">
+          <dt className="sr-only">Published on</dt>
+          <dd className="text-base font-medium text-gray-400 transition-colors group-hover:text-gray-700">
+            <time dateTime="2023-11-04">November 4, 2023</time>
+          </dd>
+        </dl>
+        <div className="col-span-7 flex items-center">
+          <div className="relative ml-4">
+            <div className="h-16 border-l border-gray-400 pr-8" />
+            <div className="absolute -left-1 top-[1.6875rem] h-2.5 w-2.5 rounded-full bg-gray-400 transition-colors group-hover:bg-gray-700" />
+          </div>
+          <h3 className="text-2xl font-medium tracking-tight text-gray-700 transition-colors group-hover:text-black">
+            Introducing New &amp; Improved Analytics for Dub
+          </h3>
+        </div>
+      </a>
+      <a
+        className="flex items-center space-x-4 rounded-lg active:bg-gray-100 md:hidden"
+        href="/blog/introducing-new-analytics"
+      >
+        <div className="relative">
+          <div className="h-16 border-l border-gray-400" />
+          <div className="absolute -left-1 top-5 h-2.5 w-2.5 rounded-full bg-gray-400" />
+        </div>
+        <div>
+          <dl>
+            <dt className="sr-only">Published on</dt>
+            <dd className="text-sm font-medium text-gray-400">
+              <time dateTime="2023-11-04">November 4, 2023</time>
+            </dd>
+          </dl>
+          <h3 className="text-lg font-medium tracking-tight text-gray-700">
+            Introducing New &amp; Improved Analytics for Dub
+          </h3>
+        </div>
+      </a>
+    </li>
+    <li>
+      <a
+        className="group hidden grid-cols-9 items-center md:grid"
+        href="/changelog/bot-clicks-not-counted"
+      >
+        <dl className="col-span-2">
+          <dt className="sr-only">Published on</dt>
+          <dd className="text-base font-medium text-gray-400 transition-colors group-hover:text-gray-700">
+            <time dateTime="2023-10-29">October 29, 2023</time>
+          </dd>
+        </dl>
+        <div className="col-span-7 flex items-center">
+          <div className="relative ml-4">
+            <div className="h-16 border-l border-gray-400 pr-8" />
+            <div className="absolute -left-1 top-[1.6875rem] h-2.5 w-2.5 rounded-full bg-gray-400 transition-colors group-hover:bg-gray-700" />
+          </div>
+          <h3 className="text-2xl font-medium tracking-tight text-gray-700 transition-colors group-hover:text-black">
+            Bot clicks are no longer counted
+          </h3>
+        </div>
+      </a>
+      <a
+        className="flex items-center space-x-4 rounded-lg active:bg-gray-100 md:hidden"
+        href="/changelog/bot-clicks-not-counted"
+      >
+        <div className="relative">
+          <div className="h-16 border-l border-gray-400" />
+          <div className="absolute -left-1 top-5 h-2.5 w-2.5 rounded-full bg-gray-400" />
+        </div>
+        <div>
+          <dl>
+            <dt className="sr-only">Published on</dt>
+            <dd className="text-sm font-medium text-gray-400">
+              <time dateTime="2023-10-29">October 29, 2023</time>
+            </dd>
+          </dl>
+          <h3 className="text-lg font-medium tracking-tight text-gray-700">
+            Bot clicks are no longer counted
+          </h3>
+        </div>
+      </a>
+    </li>
+  </ul>
+  <a
+    className="mx-auto block max-w-fit rounded-full border border-black bg-black px-4 py-1.5 text-sm text-white hover:bg-white hover:text-black"
+    href="/changelog"
+  >
+    Full changelog
+  </a>
+</div>
     </>
   );
 }
