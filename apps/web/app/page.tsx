ccb3c9139a239a2d57bacb97f771e8a1c2f4deb7
mcxleague <119989048+mcxleague@users.noreply.github.com>
2024-01-21 10:46:46 -0800

    Update page.tsx

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -440,6 +440,43 @@ export default async function Home() {
     Full changelog
   </a>
 </div>
+<div className="mx-auto w-full max-w-screen-xl px-2.5 lg:px-20 flex flex-col items-center justify-center space-y-5 pt-20 text-center">
+  <svg
+    width={191}
+    height={191}
+    viewBox="0 0 191 191"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-10 w-10 text-black"
+  >
+    <g clipPath="url(#clip0_1301_107)">
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M122 -14H144 9.17859 129.788 5.96937 122 3.72462V-14ZM122 3.72462C113.586 1.29941 104.695 0 95.5 0C42.7568 0 0 42.7568 0 95.5C0 148.243 42.7568 191 95.5 191C148.243 191 191 148.243 191 95.5C191 60.462 172.131 29.8311 144 13.2146V100V148H122V140.897C114.258 146.018 104.977 149 95 149C67.938 149 46 127.062 46 100C46 72.938 67.938 51 95 51C104.977 51 114.258 53.982 122 59.1034V3.72462Z"
+        fill="currentColor"
+        shapeRendering="geometricPrecision"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_1301_107">
+        <rect width={191} height={191} rx="95.5" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+  <h2 className="font-display text-4xl font-extrabold leading-tight text-black sm:text-5xl sm:leading-tight">
+    <span className="bg-gradient-to-r from-amber-500 via-orange-600 to-yellow-500 bg-clip-text text-transparent">
+      Boost
+    </span>
+    <br />
+    your marketing efforts
+  </h2>
+  <p className="text-center text-gray-600 sm:max-w-lg sm:text-xl">
+    See why Vercl.co is the link management infrastructure of choice for modern
+    marketing teams
+  </p>
+</div>
+
     </>
   );
 }
