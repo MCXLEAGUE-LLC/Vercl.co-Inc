4d0b3518c8ed6ca8923934af54e2a34b37c087cd
mcxleague <119989048+mcxleague@users.noreply.github.com>
2024-01-23 14:41:03 -0800

    Update page.tsx

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,14 +1,14 @@
 "use client";
 
-import Demo from "./web/ui/home/demo";
-import Globe from "./web/ui/home/globe";
-import Stats from "./web/ui/home/stats";
-import Features from "./web/ui/home/features";
-import Hero from "./web/ui/home/hero";
-import Logos from "./web/ui/home/logos";
-import OSS from "./web/ui/home/oss";
-import Testimonials from "#/ui/home/testimonials";
-import Changelog from "#/ui/home/changelog";
+import Demo from "@/web/ui/home/demo";
+import Globe from "@/web/ui/home/globe";
+import Stats from "@/web/ui/home/stats";
+import Features from "@/web/ui/home/features";
+import Hero from "@/web/ui/home/hero";
+import Logos from "@/web/ui/home/logos";
+import OSS from "@/web/ui/home/oss";
+import Testimonials from "@/ui/home/testimonials";
+import Changelog from "@/ui/home/changelog";
 import { Background } from "@dub/ui";
 
 export default async function Home() {
