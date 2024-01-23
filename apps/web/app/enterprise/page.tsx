acdc9e0055352a4716b124843f129f091ce46629
mcxleague <119989048+mcxleague@users.noreply.github.com>
2024-01-23 08:26:09 -0800

    Create page.tsx

---
+++ apps/web/app/enterprise/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { InlineSnippet } from "@dub/ui";
+import { STAGGER_CHILD_VARIANTS } from "@dub/utils";
+import Spline from "@splinetool/react-spline";
+import va from "@vercel/analytics";
+import { motion } from "framer-motion";
+import { useParams } from "next/navigation";
+import { useState } from "react";
+import { useDebounce } from "use-debounce";
+import { Background } from "@dub/ui";
+
+export default async function Enterprise() {
+  return (
+<div className="bg-gray-50">
+  <div className="bg-white py-20 sm:py-40">
+    <h1 className="mt-5 text-center font-display text-4xl font-extrabold leading-[1.15] text-black sm:text-6xl sm:leading-[1.15]">
+      Enterpise is coming soon
+    </h1>
+  </div>
+  );
+}
