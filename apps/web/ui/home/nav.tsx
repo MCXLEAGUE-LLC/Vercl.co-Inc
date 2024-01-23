8299f0de9a5d5b6b0cae07bd30a193da5f465da4
mcxleague <119989048+mcxleague@users.noreply.github.com>
2024-01-23 14:31:10 -0800

    Add files via upload

---
+++ apps/web/ui/home/nav.tsx
@@ -0,0 +1,138 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { useParams, useSelectedLayoutSegment } from "next/navigation";
+import useScroll from "#/lib/hooks/use-scroll";
+import { cn } from "#/lib/utils";
+import MaxWidthWrapper from "@/components/shared/max-width-wrapper";
+import { APP_DOMAIN, SHOW_BACKGROUND_SEGMENTS } from "#/lib/constants";
+import va from "@vercel/analytics";
+import { LogoType } from "#/ui/icons";
+
+const navItems = [
+  {
+    name: "Pricing",
+    segment: "pricing",
+  },
+  {
+    name: "Changelog",
+    segment: "changelog",
+  },
+  {
+    name: "Help Center",
+    segment: "help",
+  },
+];
+
+export default function Nav() {
+  const { domain = "dub.sh" } = useParams() as { domain: string };
+
+  const scrolled = useScroll(80);
+  const selectedLayout = useSelectedLayoutSegment();
+  const helpCenter = selectedLayout === "help";
+
+  return (
+    <div
+      className={cn(`sticky inset-x-0 top-0 z-30 w-full transition-all`, {
+        "border-b border-gray-200 bg-white/75 backdrop-blur-lg": scrolled,
+        "border-b border-gray-200 bg-white":
+          selectedLayout && !SHOW_BACKGROUND_SEGMENTS.has(selectedLayout),
+      })}
+    >
+      <MaxWidthWrapper
+        {...(helpCenter && {
+          className: "max-w-screen-lg",
+        })}
+      >
+        <div className="flex h-14 items-center justify-between">
+          <div className="flex items-center space-x-3">
+            <Link
+              href={domain === "dub.sh" ? "/" : `https://dub.sh`}
+              {...(domain !== "dub.sh" && {
+                onClick: () => {
+                  va.track("Referred from custom domain", {
+                    domain,
+                    medium: "logo",
+                  });
+                },
+              })}
+            >
+              <LogoType />
+            </Link>
+            {helpCenter && (
+              <div className="flex items-center">
+                <div className="mr-3 h-5 border-l-2 border-gray-400" />
+                <Link
+                  href="/help"
+                  className="font-display text-lg font-bold text-gray-700"
+                >
+                  Help Center
+                </Link>
+              </div>
+            )}
+          </div>
+
+          <div className="hidden items-center space-x-2 sm:flex">
+            {!helpCenter &&
+              navItems.map(({ name, segment }) => (
+                <Link
+                  id={`nav-${segment}`}
+                  key={segment}
+                  href={
+                    domain === "dub.sh"
+                      ? `/${segment}`
+                      : `https://dub.sh/${segment}`
+                  }
+                  {...(domain !== "dub.sh" && {
+                    onClick: () => {
+                      va.track("Referred from custom domain", {
+                        domain,
+                        medium: `navbar item (${segment})`,
+                      });
+                    },
+                  })}
+                  className={cn(
+                    "z-10 rounded-full px-4 py-1.5 text-sm font-medium capitalize text-gray-500 transition-colors ease-out hover:text-black",
+                    {
+                      "text-black": selectedLayout === segment,
+                    },
+                  )}
+                >
+                  {name}
+                </Link>
+              ))}
+            <Link
+              href={`${APP_DOMAIN}/login`}
+              {...(domain !== "dub.sh" && {
+                onClick: () => {
+                  va.track("Referred from custom domain", {
+                    domain,
+                    medium: `navbar item (login)`,
+                  });
+                },
+              })}
+              className="rounded-full px-4 py-1.5 text-sm font-medium text-gray-500 transition-colors ease-out hover:text-black"
+            >
+              Log in
+            </Link>
+            <Link
+              href={`${APP_DOMAIN}/register`}
+              {...(domain !== "dub.sh" && {
+                onClick: () => {
+                  va.track("Referred from custom domain", {
+                    domain,
+                    medium: `navbar item (signup)`,
+                  });
+                },
+              })}
+              className="rounded-full border border-black bg-black px-4 py-1.5 text-sm text-white transition-all hover:bg-white hover:text-black"
+            >
+              Sign Up
+            </Link>
+          </div>
+        </div>
+      </MaxWidthWrapper>
+    </div>
+  );
+}
---
+++ apps/web/ui/modal.tsx
@@ -0,0 +1,121 @@
+"use client";
+
+import { Dispatch, SetStateAction, useEffect, useRef } from "react";
+import { useRouter } from "next/navigation";
+import FocusTrap from "focus-trap-react";
+import { AnimatePresence, motion, useAnimation } from "framer-motion";
+import { cn } from "#/lib/utils";
+import { Drawer } from "vaul";
+import useWindowSize from "#/lib/hooks/use-window-size";
+
+export default function Modal({
+  children,
+  showModal,
+  setShowModal,
+  className,
+  onClose,
+  disableDefaultHide,
+}: {
+  children: React.ReactNode;
+  showModal?: boolean;
+  setShowModal?: Dispatch<SetStateAction<boolean>>;
+  className?: string;
+  onClose?: () => void;
+  disableDefaultHide?: boolean;
+}) {
+  const router = useRouter();
+
+  const closeModal = ({ dragged }: { dragged?: boolean } = {}) => {
+    if (disableDefaultHide && !dragged) {
+      return;
+    }
+    // fire onClose event if provided
+    onClose && onClose();
+
+    // if setShowModal is defined, use it to close modal
+    if (setShowModal) {
+      setShowModal(false);
+      // else, this is intercepting route @modal
+    } else {
+      router.back();
+    }
+  };
+
+  useEffect(() => {
+    function onKeyDown(e: KeyboardEvent) {
+      if (e.key === "Escape") {
+        closeModal();
+      }
+    }
+    // only add listener if modal is open
+    if (showModal) document.addEventListener("keydown", onKeyDown);
+    return () => document.removeEventListener("keydown", onKeyDown);
+  }, [showModal, closeModal]);
+
+  const { isMobile } = useWindowSize();
+
+  if (isMobile) {
+    return (
+      <Drawer.Root
+        open={showModal}
+        onOpenChange={(open) => {
+          if (!open) {
+            closeModal({ dragged: true });
+          }
+        }}
+        shouldScaleBackground
+      >
+        <Drawer.Overlay className="fixed inset-0 z-40 bg-gray-100 bg-opacity-10 backdrop-blur" />
+        <Drawer.Portal>
+          <Drawer.Content className="fixed bottom-0 left-0 right-0 z-50 mt-24 rounded-t-[10px] border-t border-gray-200 bg-white">
+            <div className="mx-auto my-3 h-1 w-12 rounded-full bg-gray-300" />
+            {children}
+          </Drawer.Content>
+          <Drawer.Overlay />
+        </Drawer.Portal>
+      </Drawer.Root>
+    );
+  }
+
+  return (
+    <AnimatePresence>
+      {(setShowModal ? showModal : true) && (
+        <FocusTrap
+          focusTrapOptions={{
+            initialFocus: false,
+            clickOutsideDeactivates: true,
+            onActivate: () => {
+              // prevent scroll outside of modal when modal is open
+              document.body.style.overflow = "hidden";
+            },
+            onDeactivate: () => {
+              // allow scroll outside of modal when modal is closed
+              document.body.style.overflow = "auto";
+            },
+          }}
+        >
+          <div className="absolute">
+            <motion.dialog
+              key="desktop-modal"
+              className={cn(
+                "fixed inset-0 z-50 m-auto hidden max-h-fit w-full max-w-md animate-scale-in overflow-hidden rounded-2xl border border-gray-200 bg-white p-0 shadow-xl md:block",
+                className,
+              )}
+            >
+              {children}
+            </motion.dialog>
+            <motion.div
+              id="modal-backdrop"
+              key="backdrop"
+              className="fixed inset-0 z-40 bg-gray-100 bg-opacity-50 backdrop-blur-md"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              onClick={() => closeModal()}
+            />
+          </div>
+        </FocusTrap>
+      )}
+    </AnimatePresence>
+  );
+}
