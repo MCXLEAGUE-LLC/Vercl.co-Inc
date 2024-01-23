a70a86a1abeadc31d73bdab148f036911bf9e6b4
mcxleague <119989048+mcxleague@users.noreply.github.com>
2024-01-23 14:54:03 -0800

    Update page.tsx

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -8,7 +8,6 @@ import Hero from "@/ui/home/hero";
 import Logos from "@/ui/home/logos";
 import OSS from "@/ui/home/oss";
 import Testimonials from "@/ui/home/testimonials";
-import Changelog from "@/ui/home/changelog";
 import { Background } from "@dub/ui";
 
 export default async function Home() {
@@ -22,7 +21,6 @@ export default async function Home() {
       <Stats />
       <Features />
       <Testimonials />
-      <Changelog />
       <OSS />
     </>
   );
