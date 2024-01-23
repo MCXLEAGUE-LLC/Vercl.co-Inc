1007161cd282ed2dc4cebefeb7a3429a8017d1ab
mcxleague <119989048+mcxleague@users.noreply.github.com>
2024-01-23 08:27:48 -0800

    Update page.tsx

--- apps/web/app/enterprise/page.tsx
+++ apps/web/app/enterprise/page.tsx
@@ -12,11 +12,15 @@ import { Background } from "@dub/ui";
 
 export default async function Enterprise() {
   return (
+<>
+    <Background />
 <div className="bg-gray-50">
   <div className="bg-white py-20 sm:py-40">
     <h1 className="mt-5 text-center font-display text-4xl font-extrabold leading-[1.15] text-black sm:text-6xl sm:leading-[1.15]">
       Enterpise is coming soon
     </h1>
   </div>
+  </div>
+</>
   );
 }
