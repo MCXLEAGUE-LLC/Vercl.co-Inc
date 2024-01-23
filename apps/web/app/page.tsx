f7856f2336d4c93743c68f5f69ecc957b5658786
mcxleague <119989048+mcxleague@users.noreply.github.com>
2024-01-23 14:36:18 -0800

    Update page.tsx

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 
-import { InlineSnippet } from "@dub/ui";
-import { STAGGER_CHILD_VARIANTS } from "@dub/utils";
-import Spline from "@splinetool/react-spline";
-import va from "@vercel/analytics";
-import { motion } from "framer-motion";
-import { useParams } from "next/navigation";
-import { useState } from "react";
-import { useDebounce } from "use-debounce";
+import Demo from "#/ui/home/demo";
+import Globe from "#/ui/home/globe";
+import Stats from "#/ui/home/stats";
+import Features from "#/ui/home/features";
+import Hero from "#/ui/home/hero";
+import Logos from "#/ui/home/logos";
+import OSS from "#/ui/home/oss";
+import Testimonials from "#/ui/home/testimonials";
+import Changelog from "#/ui/home/changelog";
 import { Background } from "@dub/ui";
 
 export default async function Home() {
