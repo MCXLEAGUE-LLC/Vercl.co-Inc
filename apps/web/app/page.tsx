de426afaf1a2660b69f9edd51bb533793564457b
mcxleague <119989048+mcxleague@users.noreply.github.com>
2024-01-23 16:48:42 -0800

    Update page.tsx

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { InlineSnippet } from "@dub/ui";
 import { STAGGER_CHILD_VARIANTS } from "@dub/utils";
 import Spline from "@splinetool/react-spline";
@@ -7,11 +9,6 @@ import { useParams } from "next/navigation";
 import { useState } from "react";
 import { useDebounce } from "use-debounce";
 import { Background } from "@dub/ui";
-
-export const metadata = constructMetadata({
-  title: "Pricing - Dub",
-});
-
 
 export default async function Home() {
   return (
