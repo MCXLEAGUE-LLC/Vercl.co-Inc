6569e0886d15ea95f65728f932e9698fecfb6b36
mcxleague <119989048+mcxleague@users.noreply.github.com>
2024-01-25 06:17:43 -0800

    Globe.tsx fix

--- packages/ui/src/globe.tsx
+++ packages/ui/src/globe.tsx
@@ -1,8 +1,18 @@
 import GlobeClient from "./globe-client";
 
+interface Coordinate {
+  latitude: number;
+  longitude: number;
+}
+
+interface Marker {
+  location: [number, number];
+  size: number;
+}
+
 export default async function Globe() {
   const response = await fetch(
-    `https://api.us-east.aws.tinybird.co/v0/pipes/coordinates.json`,
+    `https://api.us-east.tinybird.co/v0/pipes/coordinates.json`,
     {
       headers: {
         Authorization: `Bearer ${process.env.TINYBIRD_API_KEY}`,
@@ -10,17 +20,17 @@ export default async function Globe() {
       next: {
         revalidate: 43200, // every 12 hours
       },
-    },
+    }
   )
     .then((res) => res.json())
-    .then((res) => res.data);
+    .then((res) => res.data as Coordinate[]); // Explicitly type the response data
 
-  const markers = response.map(({ latitude, longitude }, idx) => {
-    return {
+  const markers: Marker[] = response.map(
+    ({ latitude, longitude }, idx) => ({
       location: [latitude, longitude],
       size: 0.075 - (0.075 / 50) * idx,
-    };
-  });
+    })
+  );
 
   return <GlobeClient markers={markers} />;
 }
