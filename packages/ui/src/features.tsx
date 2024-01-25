445f55f5a698a5636329f4a4cb861dcc8eb5da62
mcxleague <119989048+mcxleague@users.noreply.github.com>
2024-01-24 16:57:15 -0800

    Update features.tsx

--- packages/ui/src/features.tsx
+++ packages/ui/src/features.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import Link from "next/link";
-import { Airplay, Chart, Users, Photo } from "@/components/shared/icons";
-import MaxWidthWrapper from "@/components/shared/max-width-wrapper";
+import { Airplay, Chart, Users, Photo } from "../../../components/shared/icons";
+import MaxWidthWrapper from "../../../components/shared/max-width-wrapper";
 import { useState } from "react";
 import {
   Accordion,
